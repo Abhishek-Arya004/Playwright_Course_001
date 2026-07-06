a4829188dc1e407f65bc08031b216d29ee44a7f8
Abhishek kumar Arya <abhishekkumararya@Abhisheks-MacBook-Air.local>
2026-07-06 22:28:43 +0530

    Conditional code and loop code

---
+++ Batch09PM30JUN/basic_2.js
@@ -0,0 +1,49 @@
+//loops in javascript: it is a block of code which is used to perform the operation on the data,
+//  it is used to repeat the block of code for a specific number of times or until a specific condition is met.
+
+//print table  of 2
+
+//for loop: it is a block of code which is used to perform the operation on the data,
+// it is used to repeat the block of code for a specific number of times or until a specific condition is met.
+
+//for loop syntax:
+//for(initialization; condition; increment/decrement){
+//block of code
+//}
+
+//initialization: it is used to initialize the variable, it is executed only once at the beginning of the loop.
+//condition: it is used to check the condition, if the condition is true then the block of code is executed, if the condition is false then the loop is terminated.
+//increment/decrement: it is used to increment or decrement the variable, it is executed after each iteration of the loop.
+
+// for (let i = 1; i <= 10; i++) {
+//   // i++ = i + 1;
+//   console.log(i * 3);
+// }
+
+//do while loop
+
+//do while loop syntax:
+//do{
+//block of code
+//}while(condition);
+
+let j = 1;
+
+do {
+  console.log(j);
+  j++;
+} while (j <= 10);
+
+//while loop syntax:
+
+//while(condition){
+//block of code
+
+//increment/decrement
+//}
+
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
---
+++ Batch09PM30JUN/basic_3.js
@@ -0,0 +1,89 @@
+//conditional statemenets: it is a block of code which is used to perform the operation on the data, based on the condition,
+
+//if statement: it is a block of code which is used to perform the operation on the data, based on the condition,
+//  if the condition is true then the block of code is executed, if the condition is false then the block of code is not executed.
+
+//let age = 10;
+
+// if (age >= 18) {
+//   console.log("You are eligible to vote");
+// }
+
+let i = 10;
+let j = 55;
+let k = 65;
+
+// if (i > j) {
+//   console.log("i is greater than j");
+//   console.log("i is greater than j");
+//   console.log("i is greater than j");
+//   console.log("i is greater than j");
+//   console.log("i is greater than j");
+
+//   console.log("i is greater than j");
+// }
+//next line
+//if{} else{} statement: it is a block of code which is used to perform the operation on the data, based on the condition,
+
+// if (age >= 18) {
+//   console.log("You are eligible to vote");
+// } else {
+//   console.log("You are not eligible to vote");
+// }
+
+// if , if else , if else if
+// if (i > j) {
+//   if (i > k) {
+//     console.log("i is greater than j and k");
+//   } else {
+//     console.log("K is greater than j but not i");
+//   }
+// } else if (i > k) {
+//   console.log("J is greater than K and i");
+// } else {
+//   console.log("K is greater than j and i");
+// }
+
+//swich statement: it is a block of code which is used to perform the operation on the data, based on the condition,
+
+//switch (expression) {
+// case value1:
+// block of code
+// break;
+// case value2:
+// block of code
+// break;
+// default:
+// block of code
+//}
+
+let day = "8778778";
+
+switch (day) {
+  case "Monday":
+    console.log("Today is Monday");
+    break;
+  case "Tuesday":
+    console.log("Today is Tuesday");
+    break;
+  case "Wednesday":
+    console.log("Today is Wednesday");
+    break;
+  case "Thursday":
+    console.log("Today is Thursday");
+    break;
+  case "Friday":
+    console.log("Today is Friday");
+    break;
+  case "Saturday":
+    console.log("Today is Saturday");
+    break;
+  case "Sunday":
+    console.log("Today is Sunday");
+    break;
+  default:
+    console.log("Invalid day");
+}
+
+//break statement: it is keyword which is used to terminate the loop or switch statement,
+//  it is used to exit the loop or switch statement when a specific condition is met.
